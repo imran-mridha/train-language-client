1f664a9e955cf5c6ef0340ca3e142c39733ea55b
imran-mridha <mdimranmridha.pro@gmail.com>
2022-12-22 00:51:08 +0600

    Added ScrollRestoration

--- src/layout/Main.js
+++ src/layout/Main.js
@@ -2,12 +2,14 @@ import React from 'react';
 import { Outlet} from 'react-router-dom';
 import Footer from '../shared/Footer/Footer';
 import Header from '../shared/Header/Header';
+import { ScrollRestoration } from "react-router-dom";
 
 const Main = () => {
   return (
     <div>
       <Header />
       <Outlet />
+      <ScrollRestoration />
       <Footer />
     </div>
   );
